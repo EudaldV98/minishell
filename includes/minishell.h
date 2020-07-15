cf551bc0f4bd0f124f40413a2803d7263f0bf532
Jan eudald VAQUER <jvaquer@e1r2p20.42.fr>
2020-07-15 16:28:12 +0200

    echo

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 15:37:35 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/07/14 18:12:42 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/07/15 14:07:58 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # include <stdarg.h>
 # include <stdio.h>
 # include "../includes/ft_printf.h"
